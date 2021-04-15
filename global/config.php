dcfacae999b8a2169693b7234036825389f28d46
Solanny <batistasolanny026@gmail.com>
2021-04-14 14:03:54 -1000

    Proyecto

---
+++ global/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("SERVIDOR", "localhost");
+define("USUARIO", "root");
+define("PASSWORD", "");
+define("BD", "tienda");
+
+
+?>
---
+++ index.php
@@ -0,0 +1,124 @@
+<html>
+<head>
+    <title>Tienda JYKYLS</title>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    
+    <link rel="stylesheet"   href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+  </head> 	  
+	      <body>          
+          <nav>       
+
+           <div class="logo">Onfash</div>                                             
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="">Lo que todos compran</a></li>
+                        <li><a href="">Cosmeticos</a></li>
+                        <li><a href="">Productos</a></li>
+                        <li><a href="">🛒</a></li> 
+
+      <form class="form-inline my-2 my-lg-0">
+      <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-1 my-sm-0" type="submit">Buscar</button>
+    </form>    
+    
+                </div>
+                </ul> 
+                </div>      
+                </nav>
+                
+                                
+           <section >
+           </section>
+
+               <br/>
+               
+
+                <div class="container">
+    
+                <br/>
+                <div class="alert alert-success">    
+                <a class="badge badge-success" href="#">Ver mi compra</a>
+                   </div>
+                   </div>
+
+    <br/>
+   	<div class="row">
+                    
+       <div class="col-3">
+      <div class="card">
+        <img 
+        title="Vestido"
+        alt="Título"
+        class="card-img-top" 
+        src="img/1.jpg">
+
+        <div class="card-body">
+        <span>Vestido</span> 
+        <h5 class="card-title">$350.00</h5>
+          <p class="card-text">Amarillo, Azul, Rosa Size(M-6,G-8, EG-10)</p>
+          <button 
+          class="btn btn-primary"
+           name="btnAccion" 
+           value="Agregar" 
+           type="submit">
+            Agregar al carrito
+              </button>
+                </div>
+   </div>
+      </div>
+
+     
+      <div class="col-3">
+      <div class="card">
+        <img 
+        title="TT-SHIRT"
+        alt="Título"
+        class="card-img-top" 
+        src="img/3.jpg">
+
+        <div class="card-body">
+        <span>T-shirt</span> 
+        <h5 class="card-title">$250.00</h5>
+          <p class="card-text">Amarillo, Negro, Rosa, Blanco</p>
+          <button 
+          class="btn btn-primary"
+           name="btnAccion" 
+           value="Agregar" 
+           type="submit">
+            Agregar al carrito
+              </button>
+                </div>
+   </div>
+      </div>
+      <div class="col-3">
+      <div class="card">
+        <img 
+        title="Lentes de lujo"
+        alt="Título"
+        class="card-img-top" 
+        src="img/2.png">
+
+        <div class="card-body">
+        <span>Lentes de colores</span> 
+        <h5 class="card-title">$125.00</h5>
+          <p class="card-text">Rosa, Azul, Gris, Negro, Amarillo y Naranja </p>
+          <button 
+          class="btn btn-primary"
+           name="btnAccion" 
+           value="Agregar" 
+           type="submit">
+            Agregar al carrito
+              </button>
+                </div>
+   </div>
+      </div>
+         
+      
+</body>
+</html>
