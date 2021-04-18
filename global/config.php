64b15aa52a26005cbe684fff9ea81f1178170fdb
Solanny <batistasolanny026@gmail.com>
2021-04-18 05:48:25 -1000

    Encriptando datos

--- global/config.php
+++ global/config.php
@@ -1,4 +1,7 @@
 <?php
+define("KEY", "develoteca");
+define("COD", "AES-128-ECB");
+
 
 define("SERVIDOR", "localhost");
 define("USUARIO", "root");
@@ -6,4 +9,4 @@ define("PASSWORD", "");
 define("BD", "tienda");  
 
 
-?>
+?> 
