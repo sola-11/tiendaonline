268052a0d612b2daa4d831cbfc197e1b9df91b8c
Solanny <batistasolanny026@gmail.com>
2021-04-19 15:22:46 -1000

    Incluyendo cabecera de paginas

---
+++ templates/cabecera.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+   <title>ONFASH</title>
+    
+    <link rel="stylesheet"   href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+  </head> 	  
+	      <body>          
+        <nav>  
+           <div class="logo">
+             <a href="index.php">Onfash</a>
+           </div>   
+                                                    
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="2.php">Lo que todos compran</a></li>
+                        <li><a href="3.php">Cosmeticos</a></li>
+                        <li><a href="4.php">Productos</a></li>
+                        <li><a href="mostrarcarrito.php">Carrito(0)</a></li> 
+                </div>
+                </ul> 
+                </div>      
+                </nav>
+                <div class="banner-area">
+                  <h2></h2>
+                </div>
+                                
+          
+
+               <br/>
+               
+
+                <div class="container">
