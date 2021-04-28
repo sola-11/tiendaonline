9c9b16348fd74d98fb496c7876b5ae71779f25a0
Solanny <batistasolanny026@gmail.com>
2021-04-28 13:52:03 -1000

    Agregando las partes de la app

---
+++ 8.php
@@ -0,0 +1,80 @@
+<?php
+include "global/config.php";
+include "global/conexion.php";
+include 'carrito.php';
+include 'templates/cabecera.php';
+?>
+ <br/>
+                <?php if($mensaje!=""){?>
+                <div class="alert alert-success">
+                <?php echo $mensaje; ?> 
+
+                <a href="mostrarcarrito.php" class="badge badge-warning">Ver mi lista de compra</a>
+                   </div>
+                   <?php  }?>
+
+    <br/>
+   	<div class="row">
+     <?php
+      $sentencia=$pdo->prepare("SELECT * FROM ``");
+      $sentencia->execute();
+      $listaProductos=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+      //print_r($listaProductos);   
+    ?>
+    <?php foreach($listaProductos as $producto){ ?>
+
+    
+      <div class="col-3">
+      <div class="card">
+        <img 
+
+        title="<?php echo $producto['Nombre'];?>"
+        alt="<?php echo $producto['Nombre'];?>"
+        class="card-img-top" 
+        src="<?php echo $producto['Imagen'];?>"
+         data-toggle="popover"
+         data-trigger="hover"
+         data-content="<?php echo $producto['Descripción'];?>"
+        heigth="317px"
+
+         >
+
+
+        <div class="card-body">
+        <span><?php echo $producto['Nombre'];?></span> 
+        <h5 class="card-title">$<?php echo $producto['Precio'];?></h5>
+          <p class="card-text">Disponible
+          </p>
+         
+         <form action="" method="post">
+
+         <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt ($producto['ID'],COD,KEY);?>">
+         <input type="hidden" name="nombre" id="nombre" value="<?php echo openssl_encrypt ($producto['Nombre'],COD,KEY);?>">
+         <input type="hidden" name="precio" id="precio" value="<?php echo openssl_encrypt ($producto['Precio'],COD,KEY);?>">
+         <input type="hidden" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt (1,COD,KEY);?>">
+
+         <button 
+          class="btn btn-warning"
+           name="btnAccion" 
+           value="Agregar" 
+           type="submit">
+            Agregar al carrito
+              </button>
+         </form>
+          </div>
+   </div>
+   <br>
+      </div>
+      
+      <?php } ?>
+      </div>
+
+<script>
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+</script>
+
+<?php
+include 'templates/pie.php';
+?>
