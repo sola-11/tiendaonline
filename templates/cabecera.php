0401cda0ac1651850b7ea164256e06089ea54224
Solanny <batistasolanny026@gmail.com>
2021-04-28 13:45:55 -1000

    Agregando estilo al menu

--- templates/cabecera.php
+++ templates/cabecera.php
@@ -7,24 +7,28 @@
    <title>ONFASH</title>
     
     <link rel="stylesheet"   href="css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   </head> 	  
 	      <body>          
         <nav>  
            <div class="logo">
-             <a href="index.php">Onfash</a>
+             <a href="index.php"><h3>Onfash</h3></a>
            </div>   
                                                     
                     <ul>
                         <li><a href="index.php">Home</a></li>
                         <li><a href="2.php">Lo que todos compran</a></li>
                         <li><a href="3.php">Cosmeticos</a></li>
-                        <li><a href="4.php">Productos</a></li>
+                        <li><a href="">Productos</a>
+                     <ul>
+                     <li><a href="4.php">Sala</a></li>
+                     <li><a href="5.php">Cocina</a></li>
+                     <li><a href="6.php">Habitaciones</a></li>
+                     <li><a href="7.php">Belleza</a></li>
+                     <li><a href="8.php">Viaje</a></li>
+                     </ul>
+                      </li>                       
+
                         <li><a href="mostrarcarrito.php">Carrito(<?php 
                         echo (empty($_SESSION['CARRITO']))?0:count($_SESSION['CARRITO']); 
                         ?>)</a></li> 
@@ -33,7 +37,7 @@
                 </div>      
                 </nav>
                 <div class="banner-area">
-                  <h2></h2>
+                  <h2>Especial en Home y desuentos toda la semana</h2>
                 </div>
                                 
           
