05bde0556865886c9899092ee63aa577e1084ad4
Solanny <batistasolanny026@gmail.com>
2021-05-01 07:07:25 -1000

    Diseño para pago

--- pagar.php
+++ pagar.php
@@ -10,27 +10,51 @@ if($_POST){
 
     $Total=0;
     $SID=session_id();
-    $Correo="batistasolanny026@gmail.com";
+    $Correo=$_POST['email']; 
     
     foreach($_SESSION['CARRITO'] as $indice=>$producto){  
    $Total=$Total+($producto['PRECIO']*$producto['CANTIDAD']);
    } 
 
    $sentencia=$pdo->prepare("INSERT INTO `tblventas`
-    (`ID`, `ClaveTransacción`, `PaypalDatos`, `Fecha`, `Correo`, `Total`, `Status`) 
+    (`ID`, `ClaveTransaccion`, `PaypalDatos`, `Fecha`, `Correo`, `Total`, `Status`) 
     VALUES (NULL, :ClaveTransaccion, '', NOW(), :Correo, :Total, 'pendiente');");
     
-    $sentencia->bindParam(":ClaveTransacción", $SID);
+    $sentencia->bindParam(":ClaveTransaccion", $SID);
     $sentencia->bindParam(":Correo", $Correo);
     $sentencia->bindParam(":Total", $Total);
-    
     $sentencia->execute();
-  
+    $idVenta=$pdo->lastInsertId();
+
+    foreach($_SESSION['CARRITO'] as $indice=>$producto){ 
+   
+    $sentencia=$pdo->prepare("INSERT INTO `tbldetalleventa`
+     (`ID`, `IDVENTA`, `IDPRODUCTO`, `PRECIOUNITARIO`, `CANTIDAD`, `DESCARGADO`) 
+     VALUES (NULL, :IDVENTA, :IDPRODUCTO, :PRECIOUNITARIO, :CANTIDAD, '0');");
+     
+     $sentencia->bindParam(":IDVENTA", $idVenta);
+     $sentencia->bindParam(":IDPRODUCTO",$producto['ID']);
+     $sentencia->bindParam(":PRECIOUNITARIO",$producto['PRECIO']);
+     $sentencia->bindParam(":CANTIDAD",$producto['CANTIDAD']);
+     $sentencia->execute();
+
+
   echo "<h3>".$Total. "</h3>";
+   
     }
+     }
 
 ?>
-
+<div class="jumbotron text-center">
+  <h1 class="display-4">Paso Final</h1>
+  <hr class="my-4">
+  <p class="lead"> Estas apunto de pagar con paypal la cantidad de: </p>
+  <h3>$<?php echo number_format($Total,2); ?> </h3>
+  
+  <p>Los productos podrán ser descargados una vez que se procese el pago <br/>
+    <strong>(Para más información contactanos en Onfash2021@gmail.com)</strong>
+  </p>
+</div>
 
 
 <?php
