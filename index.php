8bbf52b0062f6e18c688e6693e577c4fd9cebc09
Anabel Henriquez <anabelperez026@gmail.com>
2021-05-05 16:03:27 -0500

    agregando articulosm

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include 'templates/cabecera.php';
     <br/>
    	<div class="row">
      <?php
-      $sentencia=$pdo->prepare("SELECT * FROM `articulos`");
+      $sentencia=$pdo->prepare("SELECT * FROM ");
       $sentencia->execute();
       $listaProductos=$sentencia->fetchAll(PDO::FETCH_ASSOC);
       //print_r($listaProductos);   
